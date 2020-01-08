31832c43d6da7c37edc09f6f9115e7bed91f42a1
Jayendra Kumara <jayendra@clouda.io>
2020-01-08 23:36:42 +0530

    validations added

--- react/components/AddOrganization.tsx
+++ react/components/AddOrganization.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { pathOr } from 'ramda'
+import React, { useState, useReducer } from 'react'
+import { pathOr, isEmpty, filter, propEq, reject } from 'ramda'
 import { Layout, PageHeader, PageBlock, Input, Button } from 'vtex.styleguide'
 import { useMutation } from 'react-apollo'
 import CREATE_DOCUMENT from '../graphql/createDocument.graphql'
@@ -10,7 +10,107 @@ interface Props {
   organizationCreated: Function
 }
 
+interface ErrorMessage {
+  name: string
+  isError: boolean
+  message: string
+}
+
+interface ErrorState {
+  errorMessages: ErrorMessage[]
+}
+
+type Action<K, V = void> = V extends void ? { type: K } : { type: K } & V
+
+type Actions =
+  | Action<'NAME_ERROR', { args: { name: string } }>
+  | Action<'TELEPHONE_ERROR', { args: { telephone: string } }>
+  | Action<'EMAIL_ERROR', { args: { email: string } }>
+
 const AddOrganization = (props: Props) => {
+  const initialState = {
+    errorMessages: [] as ErrorMessage[],
+  }
+
+  const reducer = (state: ErrorState, action: Actions): ErrorState => {
+    switch (action.type) {
+      case 'NAME_ERROR':
+        const isErrorN = isEmpty(action.args.name)
+        const messageN = isErrorN ? 'Name is required' : ''
+        const _errorN: ErrorMessage = {
+          ...pathOr(
+            [],
+            [0],
+            filter(propEq('name', 'NAME_ERROR'), state.errorMessages)
+          ),
+          ...{ name: 'NAME_ERROR', isError: isErrorN, message: messageN },
+        }
+        return {
+          ...state,
+          ...{
+            errorMessages: [
+              ...reject(propEq('name', 'NAME_ERROR'), state.errorMessages),
+              ...[_errorN],
+            ],
+          },
+        }
+      case 'TELEPHONE_ERROR':
+        const isErrorT = isEmpty(action.args.telephone)
+        const messageT = isErrorT ? 'Telephone is required' : ''
+        const _errorT: ErrorMessage = {
+          ...pathOr(
+            [],
+            [0],
+            filter(propEq('name', 'TELEPHONE_ERROR'), state.errorMessages)
+          ),
+          ...{ name: 'TELEPHONE_ERROR', isError: isErrorT, message: messageT },
+        }
+        return {
+          ...state,
+          ...{
+            errorMessages: [
+              ...reject(propEq('name', 'TELEPHONE_ERROR'), state.errorMessages),
+              ...[_errorT],
+            ],
+          },
+        }
+      case 'EMAIL_ERROR':
+        let isErrorE = false
+        let messageE = ''
+        if (
+          !isEmpty(action.args.email) &&
+          !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(
+            action.args.email
+          )
+        ) {
+          isErrorE = true
+        }
+
+        messageE = isErrorE ? 'Email is incorrect' : ''
+        const _errorE: ErrorMessage = {
+          ...pathOr(
+            [],
+            [0],
+            filter(propEq('name', 'EMAIL_ERROR'), state.errorMessages)
+          ),
+          ...{ name: 'EMAIL_ERROR', isError: isErrorE, message: messageE },
+        }
+        return {
+          ...state,
+          ...{
+            errorMessages: [
+              ...reject(propEq('name', 'EMAIL_ERROR'), state.errorMessages),
+              ...[_errorE],
+            ],
+          },
+        }
+      default:
+        return state
+    }
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+
   const [name, setName] = useState('')
   const [telephone, setTelephone] = useState('')
   const [address, setAddress] = useState('')
@@ -94,7 +194,20 @@ const AddOrganization = (props: Props) => {
             dataAttributes={{ 'hj-white-list': true }}
             label="Name"
             value={name}
-            onChange={(e: any) => setName(e.target.value)}
+            error={pathOr(
+              false,
+              [0, 'isError'],
+              filter(propEq('name', 'NAME_ERROR'), state.errorMessages)
+            )}
+            errorMessage={pathOr(
+              '',
+              [0, 'message'],
+              filter(propEq('name', 'NAME_ERROR'), state.errorMessages)
+            )}
+            onChange={(e: any) => {
+              setName(e.target.value)
+              dispatch({ type: 'NAME_ERROR', args: { name: e.target.value } })
+            }}
           />
         </div>
         <div className="mb5">
@@ -103,7 +216,23 @@ const AddOrganization = (props: Props) => {
             dataAttributes={{ 'hj-white-list': true }}
             label="Telephone"
             value={telephone}
-            onChange={(e: any) => setTelephone(e.target.value)}
+            error={pathOr(
+              false,
+              [0, 'isError'],
+              filter(propEq('name', 'TELEPHONE_ERROR'), state.errorMessages)
+            )}
+            errorMessage={pathOr(
+              '',
+              [0, 'message'],
+              filter(propEq('name', 'TELEPHONE_ERROR'), state.errorMessages)
+            )}
+            onChange={(e: any) => {
+              setTelephone(e.target.value)
+              dispatch({
+                type: 'TELEPHONE_ERROR',
+                args: { telephone: e.target.value },
+              })
+            }}
           />
         </div>
         <div className="mb5">
@@ -121,12 +250,37 @@ const AddOrganization = (props: Props) => {
             dataAttributes={{ 'hj-white-list': true }}
             label="Email"
             value={email}
-            onChange={(e: any) => setEmail(e.target.value)}
+            error={pathOr(
+              false,
+              [0, 'isError'],
+              filter(propEq('name', 'EMAIL_ERROR'), state.errorMessages)
+            )}
+            errorMessage={pathOr(
+              '',
+              [0, 'message'],
+              filter(propEq('name', 'EMAIL_ERROR'), state.errorMessages)
+            )}
+            onChange={(e: any) => {
+              setEmail(e.target.value)
+              dispatch({ type: 'EMAIL_ERROR', args: { email: e.target.value } })
+            }}
           />
         </div>
 
         <div className="mb5">
-          <Button variation="primary" onClick={() => createOrganization()}>
+          <Button
+            variation="primary"
+            disabled={
+              name === '' ||
+              telephone === '' ||
+              (email !== '' &&
+                pathOr(
+                  true,
+                  [0, 'isError'],
+                  filter(propEq('name', 'EMAIL_ERROR'), state.errorMessages)
+                ))
+            }
+            onClick={() => createOrganization()}>
             Save
           </Button>
         </div>
